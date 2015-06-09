d6e2e281fc34ce9405d9f77d7ba606fc7a60b46c
Tarvitz <tarvitz@blacklibrary.ru>
2015-06-09 22:05:25 +0300

    update travis

--- scripts/run_pytest.sh
+++ scripts/run_pytest.sh
@@ -3,10 +3,8 @@ ALLURE=`which allure.sh`
 COV=`which coverage`
 PYTHONPATH=$PWD $COV run --source=apps -m py.test --alluredir=db/reports/allure apps/
 if [ ! -z $ALLURE ]; then
-    echo "building allure reports"
     if [ ! $? -eq 0 ]; then
+        echo "building allure reports"
         $ALLURE generate generate db/reports/allure/ -o db/reports/reports -v 1.4.11
     fi
 fi
-echo "building coverage reports"
-$COV html
